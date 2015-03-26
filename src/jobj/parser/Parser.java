42bf86f408d7fb2618182a6ae4a2e5b1fa7bed60
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-03-26 22:00:44 +0100

    added JunitTests

--- src/jobj/parser/Parser.java
+++ src/jobj/parser/Parser.java
@@ -1,6 +1,10 @@
 package jobj.parser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import jobj.jobj.JObj;
 
@@ -12,12 +16,29 @@ public class Parser {
 	public JObj getJobj() {
 		return jobj;
 	}
+
 	public void setFile(File file) {
 		this.file = file;
 		parse();
 	}
-	
-	private void parse(){
-		//DO SOMETHING!
+
+	private void parse() {
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			
+			fr = new FileReader(file);
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				handleLine(line);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void handleLine(String line) {
+		System.out.println(line);
 	}
 }
